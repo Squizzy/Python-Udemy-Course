08ce84c6c41c9ceb9c6b031015caf108c3632f2e
Squizzy <crazy_squirrel@yahoo.com>
2018-12-17 20:00:09 +0000

    Hello World, first exercise

--- Python-Files/HelloWorld/HelloWorld.py
+++ Python-Files/HelloWorld/HelloWorld.py
@@ -1,2 +1,14 @@
 __author__ = 'dev'
 print('Hello, World!')
+print(1+2)
+print(7*6)
+print()
+print("The end")
+print("python's strings are easy to use")
+print('We can even involve "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Bruce"
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
