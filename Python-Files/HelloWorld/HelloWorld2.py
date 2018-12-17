6c5bcd951935a85bb536973c7539e3f5488ffe75
Squizzy <crazy_squirrel@yahoo.com>
2018-12-17 20:19:49 +0000

    Hello World, First exercise, part 2

--- Python-Files/HelloWorld/HelloWorld2.py
+++ Python-Files/HelloWorld/HelloWorld2.py
@@ -3,8 +3,20 @@ __author__ = 'dev'
 # name = input("please enter your name ")
 # print(greeting + ' ' + name)
 
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
+# splitString = "This string has been\nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5"
+# print(tabbedString)
 
-tabbedString = "1\t2\t3\t4\t5"
-print(tabbedString)
+print('The pet shop owner said "No, no, \'e\'s uh,... he\'s resting"')
+print("The pet show owner said \"No, no, 'e's uh,... he's resting\"")
+
+anotherSplitString = """This string has been
+split over \
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's uh,... he's resting"''')
+
+print("""The pet shop owner said "No, no, 'e's uh,... he's resting" """)
