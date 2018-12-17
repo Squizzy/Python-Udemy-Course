a5cb7ab27faa32c779561b6ae6d23a420d2df7f8
Squizzy <crazy_squirrel@yahoo.com>
2018-12-17 20:07:34 +0000

    Hello World, First exercise, part 2

--- Python-Files/HelloWorld/HelloWorld2.py
+++ Python-Files/HelloWorld/HelloWorld2.py
@@ -1,4 +1,7 @@
 __author__ = 'dev'
-greeting = "Hello"
-name = input("please enter your name ")
-print(greeting + ' ' + name)
+# greeting = "Hello"
+# name = input("please enter your name ")
+# print(greeting + ' ' + name)
+
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
