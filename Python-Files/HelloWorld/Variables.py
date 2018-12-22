7b99a7700af42b9fa8fb9409ea14a6463f70ac49
Squizzy <crazy_squirrel@yahoo.com>
2018-12-22 23:41:19 +0100

    Variables Part 1 till end

--- Python-Files/HelloWorld/Variables.py
+++ Python-Files/HelloWorld/Variables.py
@@ -1,12 +1,35 @@
-__author__ = 'dev'
-greeting = "Bruce"
-_myName = "Tim"
-Tim45 = "Good"
-Tim_Was_57 = "Hello"
-Greeting = "There"
-print(Tim_Was_57 + ' ' + greeting)
+# __author__ = 'dev'
+# greeting = "Bruce"
+# _myName = "Tim"
+# Tim45 = "Good"
+# Tim_Was_57 = "Hello"
+# Greeting = "There"
+# print(Tim_Was_57 + ' ' + greeting)
+#
+# age = 24
+# print(age)
+#
+# print(greeting + age)
 
-age = 24
-print(age)
+a = 12
+b = 3
+print(a +b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
 
-print(greeting + age)
+for i in range(1,4):
+    print(i)
+
+# for i in range(1,a/b):
+#     print(i)
+
+for i in range(1,a//b):
+    print(i)
+
+print(a+b/3-4*12)
+print(8/2*3)
+print(8 *3 /2)
+print((((a+b)/3)-4)*12)
