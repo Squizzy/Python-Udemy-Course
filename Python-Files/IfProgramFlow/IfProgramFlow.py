59160c6fe86084202e0a63ac7a059db7a8cf1291
Squizzy <crazy_squirrel@yahoo.com>
2018-12-23 01:38:17 +0100

    IfProgramFlow

--- Python-Files/IfProgramFlow/IfProgramFlow.py
+++ Python-Files/IfProgramFlow/IfProgramFlow.py
@@ -9,18 +9,36 @@ __author__ = 'dev'
 # else:
 #     print("Please come back in {0} years".format(18-age))
 
+# print("Please guess a number between 1 and 10: ")
+# guess = int(input())
+#
+# if guess < 5:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > 5:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time")
+
+
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < 5:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly")
-elif guess > 5:
-    print("Please guess lower")
+if guess != 5:
+    if guess < 5:
+        print("Please guess higher")
+    else:  # guess must be greater than 5
+        print("Please guess lower")
+
     guess = int(input())
     if guess == 5:
         print("Well done, you guessed it")
