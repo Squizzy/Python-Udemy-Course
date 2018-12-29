e9ecb01fb67806d55d2830223da73c1f9f21c45e
Squizzy <crazy_squirrel@yahoo.com>
2018-12-29 17:19:02 +0100

    Augmented Assigment

--- Python-Files/AugmentedAssignment/augmentedassigment.py
+++ Python-Files/AugmentedAssignment/augmentedassigment.py
@@ -9,3 +9,33 @@ for i in range(0, len(number)):
 
 newNumber = int(cleanedNumber)
 print("The number is {}".format(newNumber))
+
+
+x = 23
+x += 1
+print(x)
+
+x -= 4
+print(x)
+
+x *= 5
+print(x)
+
+x /= 4
+print(x)
+
+x **= 2
+print(x)
+
+x %= 60
+print(x)
+
+
+greeting = "Good "
+greeting += "Morning"
+print(greeting)
+
+greeting *= 5
+print(greeting)
+
+
