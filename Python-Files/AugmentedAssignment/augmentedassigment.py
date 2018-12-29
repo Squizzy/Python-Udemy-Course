e83b63e1d75f3a47ea4f9da04c704661cd279dac
Squizzy <crazy_squirrel@yahoo.com>
2018-12-29 17:23:00 +0100

    Augmented Assigment

--- Python-Files/AugmentedAssignment/augmentedassigment.py
+++ Python-Files/AugmentedAssignment/augmentedassigment.py
@@ -1,41 +1,51 @@
 __author__ = 'dev'
-number = "9,223,372,036,854,775,807"
-cleanedNumber = ''
+# number = "9,223,372,036,854,775,807"
+# cleanedNumber = ''
+#
+# for i in range(0, len(number)):
+#     if number[i] in '0123456789':
+#         # cleanedNumber = cleanedNumber + number[i]
+#         cleanedNumber += number[i]
+#
+# newNumber = int(cleanedNumber)
+# print("The number is {}".format(newNumber))
+#
+#
+# x = 23
+# x += 1
+# print(x)
+#
+# x -= 4
+# print(x)
+#
+# x *= 5
+# print(x)
+#
+# x /= 4
+# print(x)
+#
+# x **= 2
+# print(x)
+#
+# x %= 60
+# print(x)
+#
+#
+# greeting = "Good "
+# greeting += "Morning"
+# print(greeting)
+#
+# greeting *= 5
+# print(greeting)
 
-for i in range(0, len(number)):
-    if number[i] in '0123456789':
-        # cleanedNumber = cleanedNumber + number[i]
-        cleanedNumber += number[i]
 
-newNumber = int(cleanedNumber)
-print("The number is {}".format(newNumber))
+# ## Assignment
+number = 5
+multiplier = 8
+answer = 0
 
+# add your loop after this comment
+for i in range(0,multiplier):
+    answer += number
 
-x = 23
-x += 1
-print(x)
-
-x -= 4
-print(x)
-
-x *= 5
-print(x)
-
-x /= 4
-print(x)
-
-x **= 2
-print(x)
-
-x %= 60
-print(x)
-
-
-greeting = "Good "
-greeting += "Morning"
-print(greeting)
-
-greeting *= 5
-print(greeting)
-
-
+print(answer)
