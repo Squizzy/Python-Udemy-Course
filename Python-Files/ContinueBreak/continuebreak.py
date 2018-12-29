0913caf9de87bd35906da5719faef0dfdf6eb251
Squizzy <crazy_squirrel@yahoo.com>
2018-12-29 16:46:19 +0100

    Contine, Break, Else

--- Python-Files/ContinueBreak/continuebreak.py
+++ Python-Files/ContinueBreak/continuebreak.py
@@ -15,16 +15,31 @@ __author__ = 'dev'
 #         break
 #     print("Buy " + item)
 
-meal = ["egg", "bacon", "spam", "sausages"]
-nasty_food_item = ''
+# ## Else
+# meal = ["egg", "bacon", "spam", "sausages"]
+# nasty_food_item = ''
+#
+# for item in meal:
+#     if item == "spam":
+#         nasty_food_item = item
+#         break
+# else:  # else for the "for" loop in case of no break
+#     # nasty_food_item = '' could be defined here
+#     print("I'll have a plate for that, then, please")
+#
+# if nasty_food_item:
+#     print("Can't I have anything without spam in it")
 
-for item in meal:
-    if item == "spam":
-        nasty_food_item = item
-        break
-else:  # else for the "for" loop in case of no break
-    # nasty_food_item = '' could be defined here
-    print("I'll have a plate for that, then, please")
 
-if nasty_food_item:
-    print("Can't I have anything without spam in it")
+# ## Exercise # break
+# Modify this loop to stop when i is exactly divisible by 11
+# for i in range(0, 100, 7):
+#     print(i)
+#     if i % 11 == 0 and i > 0:
+#         break
+
+# ## Exercise # continue
+for i in range(0, 21):
+    if i == 0 or i % 3 == 0 or i % 5 == 0:
+        continue
+    print(i)
