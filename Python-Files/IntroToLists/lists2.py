1047e1266f926ab8784a5d839348fcd6dbba3ede
Squizzy <crazy_squirrel@yahoo.com>
2019-01-07 18:17:29 +0000

    Intro to Lists

---
+++ Python-Files/IntroToLists/lists2.py
@@ -0,0 +1,15 @@
+# list_1 = []
+# list_2 = list()  # list constructor
+#
+# print("List 1: {}".format(list_1))
+# print("List 2: {}".format(list_2))
+#
+# if list_1 == list_2:
+#     print("Lists are equal")
+#
+# print(list("the lists are equal"))
+even = [2, 4, 6, 8]
+
+another_even = even
+another_even.sort(reverse=True)
+print(even)
