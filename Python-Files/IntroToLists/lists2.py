06b2c7763f4015cc27584f6535316d251fb2c293
Squizzy <crazy_squirrel@yahoo.com>
2019-01-14 17:07:07 +0000

    Intro to Lists - 2

--- Python-Files/IntroToLists/lists2.py
+++ Python-Files/IntroToLists/lists2.py
@@ -8,8 +8,30 @@
 #     print("Lists are equal")
 #
 # print(list("the lists are equal"))
-even = [2, 4, 6, 8]
+# even = [2, 4, 6, 8]
+#
+# another_even = list(even)
+#
+# print(another_even is even)  # compares that the list are the same data
+# print(another_even == even)  # compares that the content is the same
+#
+# another_even = sorted(even, reverse=True)
+#
+# print(another_even is even)  # compares that the list are the same data
+# print(another_even == even)  # compares that the content is the same
+#
+#
+# another_even.sort(reverse=True)
+# print(even)
+# print(another_even)
 
-another_even = even
-another_even.sort(reverse=True)
-print(even)
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = [even, odd]
+print("numbers: {}".format(numbers))
+
+for numbers_set in numbers:
+    print("number_set: {}".format(numbers_set))
+    for value in numbers_set:
+        print("value: {}".format(value))
