454123740e8b0b10269e9ecf5ec227b2f1e32590
Squizzy <crazy_squirrel@yahoo.com>
2019-01-14 17:38:30 +0000

    Iterators, including challenge

---
+++ Python-Files/Iterator/iterator_challenge1.py
@@ -0,0 +1,14 @@
+# Create a list of items (you may use either strings or numbers in the list),
+# then create an iterator using the iter() function.
+#
+# Use a for loop to loop "n" times, where n is the number of items in your list.
+# Each time round the loop, use next() on your list to print the next item.
+#
+# hint: use the len() function rather than counting the number of items in the list.
+
+string = "12345567890"
+
+item = iter(string)
+
+for i in range(0, len(string)):
+    print(next(item))
