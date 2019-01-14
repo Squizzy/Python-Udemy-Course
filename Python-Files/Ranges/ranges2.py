4f2e0798abde4f04e1ea83494326d42976c0aa03
Squizzy <crazy_squirrel@yahoo.com>
2019-01-14 18:45:18 +0000

    Ranges 1, 2 and challenge

---
+++ Python-Files/Ranges/ranges2.py
@@ -0,0 +1,33 @@
+# decimals = range(0, 100)
+# my_range = decimals[3:40:3]
+# print(my_range == range(3, 40, 3))
+# print(range(0, 5, 2) == range(0, 6, 2))
+# print(list(range(0, 5, 2)))
+# print(list(range(0, 6, 2)))
+#
+# r = range(0, 100)
+# print(r)
+#
+# for i in r[::-2]:
+#     print("r: {}".format(i))
+#
+# for i in range(99, 0, -2):
+#     print("range: {}".format(i))
+#
+#
+# print("=" *50)
+#
+# print(range(0, 100)[::-2] == range(99, 0, -2))
+#
+# print("now inverse range")
+#
+# for i in range(0, 100, -2):  # no result
+#     print("inverse range: {}".format(i))
+#
+
+backString = "egaugnal lufrewop yrev a si nohtyP"
+print(backString[::-1])
+
+r = range(0, 10)
+for i in r[::-1]:
+    print(i)
