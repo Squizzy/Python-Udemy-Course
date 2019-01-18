81891e7f23acc9f08460a8fd9d6e84cbb6abad9f
Squizzy <crazy_squirrel@yahoo.com>
2019-01-18 16:25:25 +0000

    Modules import 2 - standard python library

---
+++ ModulesImport/examplestandardlibrary.py
@@ -0,0 +1,22 @@
+# print(dir())  # prints all the functions imported
+#
+# # print(dir(__builtins__))  # prints all the functions imported
+#
+# for m in dir(__builtins__):
+#     print(m)
+
+import shelve
+
+# for m in dir(shelve):
+#      print(m)
+
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != "_":
+#         print(obj)
+
+### getting help on modules
+# help(shelve)
+
+import random
+
+help(random.randint)
