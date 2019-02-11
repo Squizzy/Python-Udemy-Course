03967699bcb54fc469498dfaa38e17ffffdd5a5f
Squizzy <crazy_squirrel@yahoo.com>
2019-02-11 19:24:07 +0000

    Date & Time part 1

--- Python-Files/DateTime/elapsedtimegame.py
+++ Python-Files/DateTime/elapsedtimegame.py
@@ -16,4 +16,4 @@ end_time = my_timer()
 print("Started at " + time.strftime("%X", time.localtime(start_time)))
 print("Ended at " + time.strftime("%X", time.localtime(end_time)))
 
-print("Your reaction time was {} seconds".format(end_time - start_time))
+print("Your reaction time was {} seconds!".format(end_time - start_time))
