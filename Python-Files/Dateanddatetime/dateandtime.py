b01faaee5039d60be0c999ea12aeb2dd509c1d5d
Squizzy <crazy_squirrel@yahoo.com>
2019-02-18 10:30:01 +0000

    Datetime

---
+++ Python-Files/Dateanddatetime/dateandtime.py
@@ -0,0 +1,19 @@
+# import time
+#
+# print("The epoch starts at " + time.strftime('%c', time.gmtime(0)))
+# print("The current timezone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
+#
+# if time.daylight != 0:
+#     print("\tDaylight Saving Time is in effect for this location")
+#     print("\tthe DST timezone is " + time.tzname[1])
+#
+# print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+# print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+# print(time.tzname)
+# print(time.daylight)
+
+import datetime
+
+print(datetime.datetime.today())
+print(datetime.datetime.now())      # allows to specify a timezone (using txinfo, and more precise
+print(datetime.datetime.utcnow())
